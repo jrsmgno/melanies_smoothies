06d46c5373e39b58ed75db147eed47da9c79fd13
Jana Magno <113675362+jrsmgno@users.noreply.github.com>
2024-10-12 02:01:23 +0800

    Add fruityvice contents in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,12 @@ ingredients_list = st.multiselect(
     , my_dataframe
     , max_selections=5
 )
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
 if ingredients_list:
     #st.write(ingredients_list)
     #st.text(ingredients_list)
